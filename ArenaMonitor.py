6f8ebad8a946191277c66ee0a456d9f5f4165fbf
xiaodx <admin@hareru.moe>
2021-01-10 21:35:08 +0800

    improve

--- ArenaMonitor.py
+++ ArenaMonitor.py
@@ -24,6 +24,11 @@ def init_db(db_dir, db_name='YuKi.sqlite'):
                       decode=json.loads,
                       autocommit=True)
 
+def search_max(now:int):
+    if now<=69:
+        return max(now-10,1)
+    return int(now*0.85)
+
 class ArenaMonitor:
     def __init__(self, viewer_id, uid, access_key):
         self.config = get_config()
@@ -40,12 +45,12 @@ class ArenaMonitor:
             else:
                 logging.debug("Client loging cd")
 
-    async def get_profile(self, target_id):
+    async def get_profile(self, target_uid:int):
         await self.do_login()
         if not self.Client.ready:
             logging.error("BCRClient not ready,get profile fail")
             return {}
-        profile = await self.Client.Callapi('profile/get_profile',{'target_viewer_id':target_id})
+        profile = await self.Client.Callapi('profile/get_profile',{'target_viewer_id':target_uid})
         rec={}
         rec['time']=time.time()
         rec['arena_rank']=profile['user_info']['arena_rank']
@@ -53,7 +58,7 @@ class ArenaMonitor:
         return rec
 
 
-    async def add_uid(self, target_uid, qqid):
+    async def add_uid(self, target_uid:int, qqid:int):
         rec = await self.get_profile(target_uid)
         if rec == {}:
             logging.error("empty profile, add fail")
@@ -65,28 +70,52 @@ class ArenaMonitor:
         data['rec']=rec
         data['qqid']=qqid
         self.db[target_uid]=data
+
+    async def remove_uid(self, target_uid:int):
+        if str(target_uid) not in list(self.db.keys()):
+            logging.error("uid item not exist")
+            return
+        self.db.pop(target_uid)
+
         
-    async def mention(self,qqid,type,prev,now):
-        print("{}'s {} decrease from {} to {}".format(qqid,type,prev,now))
+    async def mention_test(self,qqid:int,type:str,prev:int,now:int,delta):
+        info_str=f"{qqid}'s {type} decrease from {prev} to {now}"
+        if prev < search_max(now) and delta <= self.config['elevator_timer']:
+            info_str += f"in {int(delta)}S\nCurrent search max is {search_max(now)}, elevator detected"
+        print(info_str)
+        return {'qqid':qqid, 'type':type, 'prev':prev, 'now':now, 'str':info_str}
         
-    async def update_profile(self, target_uid):
+    async def update_profile(self, target_uid:int, mention_func=mention_test):
+        mention_info=[]
         rec = await self.get_profile(target_uid)
         data=self.db[target_uid]
         prev=data['rec']
+        delta=rec['time']-prev['time']
         if rec['arena_rank'] > prev['arena_rank']:
-            await self.mention(data['qqid'],'jjc',prev['arena_rank'],rec['arena_rank'])
+            mention_info.append(await self.mention_func(data['qqid'],'jjc',prev['arena_rank'],rec['arena_rank']),delta)
         if rec['grand_arena_rank'] > prev['grand_arena_rank']:
-            await self.mention(data['qqid'],'pjjc',prev['grand_arena_rank'],rec['grand_arena_rank'])
+            mention_info.append(await self.mention_func(data['qqid'],'pjjc',prev['grand_arena_rank'],rec['grand_arena_rank']),delta)
         data['rec']=rec
         self.db[target_uid]=data
+        return mention_info
 
     async def update_all(self):
+        mention_info=[]
         for uid in self.db.keys():
-            await self.update_profile(int(uid))
+            mention_info.extend(await self.update_profile(int(uid)))
+        return mention_info
 
+    def bind_status(self,target_qqid:int):
+        bind_list=[]
+        for item in self.db.items():
+            if item[1]['qqid']==target_qqid:
+                bind_list.append((uid,data))
+        return bind_list
+
+def aaa():
+    a=ArenaMonitor(1160936629251, '267364644', '771c02865f3ab18e29381d0de5aac04e_sh')
+    asyncio.run(a.do_login())
+    asyncio.run(a.update_all())
+    return a
 #from ArenaMonitor import *
-#a=ArenaMonitor(1160936629251, '267364644', '771c02865f3ab18e29381d0de5aac04e_sh')
-#asyncio.run(a.do_login())
-#asyncio.run(a.update_all())
-
 
--- __init__.py
+++ __init__.py
@@ -1,29 +1,31 @@
 from os import path
 import asyncio
 import datetime
-import requests
-import ast
-import re
-import hashlib
-import base64
 
 import hoshino
 import asyncio
-from .ArenaMonitor import ArenaMonitor
+from .ArenaMonitor import ArenaMonitor, get_config
 
 sv = hoshino.Service('YuKi', visible=False)
 
-yuki=ArenaMonitor(1160936629251, '267364644', '771c02865f3ab18e29381d0de5aac04e_sh')
+config = get_config()
+yuki=ArenaMonitor(config['_viewerid'], config['_uid'], config['_access_key'])
 asyncio.run(yuki.do_login())
 
-@sv.scheduled_job('cron', minute='*/1')
-async def schedule_update_all_profile():
-    await yuki.update_all()
+@sv.scheduled_job('interval', seconds=config['refresh_cd'])
+async def scheduled_update():
+    mention_info=await yuki.update_all()
+    for info in mention_info:
+        await hoshino.get_bot().send_msg(user_id=info['qqid'], message=info['str'])
 
-@sv.on_fullmatch('YuKi_update')
-async def manual_update_all_profile(bot, ev):
-    await yuki.update_all()
-    await bot.send(ev, "YuKi updated {} users' profile".format(len(list(yuki.db.keys()))))
+
+@sv.on_fullmatch('YuKi_update', permission=SUPERUSER)
+async def manual_update(bot, ev):
+    mention_info=await yuki.update_all()
+    await bot.send(ev, f"YuKi updated {len(list(yuki.db.keys()))} users' profile, generate {len(mention_info)} reminds")
+    for info in mention_info:
+        await hoshino.get_bot().send_msg(user_id=info['qqid'], message=info['str'])
+
 
 @sv.on_prefix('YuKi_add')
 async def add_watch_list(bot, ev):
@@ -37,6 +39,12 @@ async def add_watch_list(bot, ev):
     if len(str(uid)) != 13:
         await bot.send(ev,'uid length not match')
         return
+    if ev['user_id']!=qqid and not hoshino.priv.check_priv(ev ,hoshino.priv.ADMIN):
+        await bot.send(ev, "Only admin can bind to other's qq")
+        return
     await yuki.add_uid(uid,qqid)
     await bot.send(ev, "add fin")
+
+#todo
+#switch to send(bot, ev, message, ensure_private=True)
 
